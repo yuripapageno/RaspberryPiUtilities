e1e3ff8317468f15745b9d102ceb49dff3a0c130
yuripapageno <yuripapageno@users.noreply.github.com>
2014-10-22 16:25:23 +0900

    Create report_status.py
    the first version.
    tell cpu temparature and local time.

---
+++ report_status.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import datetime
+
+
+def readcputemp():
+    f = open("/sys/class/thermal/thermal_zone0/temp","r")
+    line = f.readline()
+    f.close()
+    cputemp = '%.1f' % (float(line) / 1000.0)
+    return cputemp
+
+
+def main():
+    cputemp = readcputemp()
+    now = datetime.datetime.now()
+    timestamp = now.strftime('%Y/%m/%d %H:%M:%S')
+    message = 'Raspberry Pi B+ cpu=' + cputemp + '℃ local time=' + timestamp
+    print message
+
+
+if __name__ == '__main__':
+    main()
