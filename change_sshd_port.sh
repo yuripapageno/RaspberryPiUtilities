83b3b4c8fbca05dddf65bcde2e0521255475bb4b
yuripapageno <journal@broucek.small.jp>
2014-10-24 10:49:15 +0900

    configファイルの更新ができない。

--- change_sshd_port.sh
+++ change_sshd_port.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 # -*- coding: utf-8 -*-
 
-PEPLINE=`Port $1`
-sudo sed -e "s/^Port.*/$PEPLINE/g" test.conf
-#sudo sed -e "s/^Port.*\n/Port $1\n/g" /etc/ssh/sshd_config
+sudo sed -e "s/^Port.*/Port 23456/g" test.conf > temp.conf
+sudo cat temp.conf > /etc/ssh/sshd_config
+sudo rm temp.conf
 
 #sudo vi /etc/ssh/sshd_config
 sudo service ssh restart
-
-
