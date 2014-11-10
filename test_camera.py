f7faadf3669c1311858d39c26c6eac955447ad78
yuripapageno <journal@broucek.small.jp>
2014-11-10 17:45:32 +0900

    フレーム間差分が大きい時にキャプチャ映像を残すスクリプト。とりあえず。

--- test_camera.py
+++ test_camera.py
@@ -5,23 +5,28 @@ import numpy as np
 
 SAVE_PATH = '/run/shm/ramdisk/'
 
-def is_moving(im1, im2, th):
+
+def is_moving(image1, image2, th):
+    im1 = cv2.cvtColor(image1, cv2.COLOR_RGB2GRAY)
+    im2 = cv2.cvtColor(image2, cv2.COLOR_RGB2GRAY)
     d1 = cv2.absdiff(im1, im2)
     sum = d1.sum()
+    print('th=' + str(th) + ' : sum=' + str(sum))
     return (th < sum)
 
 
 def main():
     save_no = 0
-    save_max = 10
+    save_max = 30
     cam = cv2.VideoCapture(0)
     imwidth = cam.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH)
     imheight = cam.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT)
-    th = imwidth * imheight * 255.0 * 0.3
-    im1 = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
+    print('capture size:%dx%d' % (imwidth, imheight))
+    th = imwidth * imheight * 255.0 * 0.03
+    ret, im1 = cam.read()
 
     while True:
-        im2 = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
+        ret, im2 = cam.read()
         # フレーム間差分計算
         if (True == is_moving(im1, im2, th)):
             filename = 'moving_%04d.jpg' % save_no
@@ -38,50 +43,13 @@ def main():
 
 def main0():
     cam = cv2.VideoCapture(0)
-    image = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
-    filename = 'main0.jpg'
-    cv2.imwrite(SAVE_PATH + filename, image)
-    print('take ' + filename)
-
-
-def frame_sub(im1, im2, im3, th, blur):
-    d1 = cv2.absdiff(im3, im2)
-    d2 = cv2.absdiff(im2, im1)
-    diff = cv2.bitwise_and(d1, d2)
-    # 差分が閾値より小さければTrue
-    mask = diff < th
-    # 背景画像と同じサイズの配列生成
-    im_mask = np.empty((im1.shape[0], im1.shape[1]), np.uint8)
-    im_mask[:][:] = 255
-    # Trueの部分（背景）は黒塗り
-    im_mask[mask] = 0
-    # ゴマ塩ノイズ除去
-    im_mask = cv2.medianBlur(im_mask, blur)
-
-    return im_mask
-
-
-def main_org():
-    cam = cv2.VideoCapture(0)
-    im1 = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
-    im2 = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
-    im3 = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
- 
-    while True:
-        # フレーム間差分計算
-        im_fs = frame_sub(im1, im2, im3, 5, 7)
-#       cv2.imshow('Input',im2)
-#       cv2.imshow('Motion Mask',im_fs)
-        im1 = im2
-        im2 = im3
-        im3 = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
-        key = cv2.waitKey(10)
-        # Escキーが押されたら
-        if key == 27:
-            cv2.imwrite(SAVE_PATH + 'input.jpg', im3)
-            cv2.imwrite(SAVE_PATH + 'frame_sub.jpg', im_fs)
-            cv2.destroyAllWindows()
-            break
+    ret, image = cam.read()
+    if ret:
+        filename = 'main0.jpg'
+        cv2.imwrite(SAVE_PATH + filename, image)
+        print('take ' + filename)
+    else:
+        print('capture error.')
 
 
 if __name__ == '__main__':
